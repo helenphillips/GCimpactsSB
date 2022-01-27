a6cfc91619b78e19840a0ca4de5de284d977db15
Helen Phillips <helenphillips@users.noreply.github.com>
2022-01-27 14:11:01 -0400

    dealing with zeros in effect sizes

--- 04_EffectSizes.R
+++ 04_EffectSizes.R
@@ -2,6 +2,7 @@
 
 
 library(metafor)
+library(ggplot2)
 
 # setwd("~/WORK/GCimpactsSB")
 setwd("C:/Users/helenp/WORK/GCimpactsSB")
@@ -19,7 +20,7 @@ abundance <- DataCleaned[which(DataCleaned$Measurement == "Abundance"),] # 2024 
 # Sometimes zeros were missing that should actually be zeros
 abundance$Control_SD[which(abundance$Control_mean == 0 & is.na(abundance$Control_SD))] <- 0
 abundance$Treatment_SD[which(abundance$Treatment_mean == 0 & is.na(abundance$Treatment_SD))] <- 0
-
+# 2395
 # there are some climate cases that have no SDs
 abundance <- abundance[which(!(is.na(abundance$Control_SD))),] 
 abundance <- abundance[which(!(is.na(abundance$Treatment_SD))),] # 2378
@@ -33,19 +34,30 @@ abundance$Control_SD[which(abundance$ID == 1857 & abundance$Control_SD == 0)] <-
 abundance$Treatment_SD[which(abundance$ID == 1857 & abundance$Treatment_SD == 0)] <- 0.01
 
 
+## How many cases (and which drivers) now have zeros
+nrow(abundance[which(abundance$Control_mean == 0),]) # 129
+nrow(abundance[which(abundance$Control_SD   == 0),]) # 150
+
+table(abundance$driver[which(abundance$Control_SD   == 0)]) # Mainly lui, nutrient enrichment and pollution
+
+
+nrow(abundance[which(abundance$Treatment_mean  == 0),]) # 159
+nrow(abundance[which(abundance$Treatment_SD  == 0),]) # 179
+table(abundance$driver[which(abundance$Treatment_SD   == 0)]) # Mainly lui,  nutrient enrichment and pollution
+
+
+
 EffectSizes <- escalc(measure = "ROM", # log response ratio ("ROM" in metafor)
                  m2i = Control_mean, # group 2 corresponds to the control group
                  sd2i = Control_SD,
                  n2i = Control_N,
-                 
                  m1i = Treatment_mean, # group 1 is the treatment group
                  sd1i = Treatment_SD,
                  n1i = Treatment_N,
-                 
                  data = abundance)
 
 
-## get a warning message here that I should deal with
+## get a warning message here because of the zeros, which gets dealt with shortly
 
 
 
@@ -85,10 +97,24 @@ EffectSizes$VarLRR[which(EffectSizes$driver == "Invasives" & is.na(EffectSizes$v
 EffectSizes$VarLRR[which(EffectSizes$driver == "LUI" & is.na(EffectSizes$vi))] <- median(EffectSizes$vi[EffectSizes$driver == "LUI"], na.rm = TRUE)
 EffectSizes$VarLRR[which(EffectSizes$driver == "NutrientEnrichment" & is.na(EffectSizes$vi))] <-median(EffectSizes$vi[EffectSizes$driver == "NutrientEnrichment"], na.rm = TRUE)
 
+# Check this is acceptable
+EffectSizes$VarLRR[which(EffectSizes$driver == "Pollution" & EffectSizes$VarLRR == 0)] <- median(EffectSizes$vi[EffectSizes$driver == "Pollution"], na.rm = TRUE)
+EffectSizes$VarLRR[which(EffectSizes$driver == "Climate" & EffectSizes$VarLRR == 0)] <- median(EffectSizes$vi[EffectSizes$driver == "Climate"], na.rm = TRUE)
+EffectSizes$VarLRR[which(EffectSizes$driver == "Invasives" & EffectSizes$VarLRR == 0)] <- median(EffectSizes$vi[EffectSizes$driver == "Invasives"], na.rm = TRUE)
+EffectSizes$VarLRR[which(EffectSizes$driver == "LUI" & EffectSizes$VarLRR == 0)] <- median(EffectSizes$vi[EffectSizes$driver == "LUI"], na.rm = TRUE)
+EffectSizes$VarLRR[which(EffectSizes$driver == "NutrientEnrichment" & EffectSizes$VarLRR == 0)] <-median(EffectSizes$vi[EffectSizes$driver == "NutrientEnrichment"], na.rm = TRUE)
+
+
+
+
+
 
 
 # Save data
 write.csv(EffectSizes, "Data/03_Data/EffectSizes.csv")
+
+
+
 
 
 EffectSizes$UniqueID <- paste(EffectSizes$ID, EffectSizes$Case_ID, EffectSizes$driver)
@@ -96,8 +122,8 @@ EffectSizes$UniqueID <- paste(EffectSizes$ID, EffectSizes$Case_ID, EffectSizes$d
 mod.1<-rma.mv(
   yi=LRR,
   V=VarLRR, 
-  mods=~driver,
-  random= ~1|ID,
+  mods=~driver-1,
+  random= ~1|ID/UniqueID,
   struct="CS",
   method="ML",
   digits=4,
@@ -105,6 +131,15 @@ mod.1<-rma.mv(
 
 qqnorm(residuals(mod.1,type="pearson"),main="QQ plot: residuals")
 qqline(residuals(mod.1,type="pearson"),col="red")
+
+
+## Coloured qq plot https://stackoverflow.com/questions/42678858/q-q-plot-with-ggplot2stat-qq-colours-single-group
+library(broom) ## for augment()
+dda <- cbind(augment(mod.1),f=EffectSizes$driver)
+dda = cbind(dda, setNames(qqnorm(dda$.resid, plot.it=FALSE), c("Theoretical", "Sample")))
+
+ggplot(dda) + 
+  geom_point(aes(x=Theoretical, y=Sample, colour=f))
 
 
 
@@ -139,14 +174,3 @@ p
 
 
 
-test <- EffectSizes[-(which(EffectSizes$VarLRR > 10)),]
-
-mod.test<-rma.mv(
-  yi=LRR,
-  V=VarLRR, 
-  mods=~driver,
-  random= ~1|ID,
-  struct="CS",
-  method="ML",
-  digits=4,
-  data=test)
